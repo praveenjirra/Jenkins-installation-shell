8c3e1de5f6bb707cf11837b1d5c67b2da8d4a80d
root <root@ip-172-31-29-2.us-west-2.compute.internal>
2019-08-05 06:07:24 +0000

    Jenkins-installation-shell

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This script install Jenkins in your Ubuntu System
+sudo apt-get update -y 
+
+sudo apt-get install openjdk-8-jdk -y
+
+# Install Jenkins
+## Before install is necessary to add Jenkins to trusted keys and source list
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
