673a91a7bbb071456076580f276bd0a375463181
Praveen <praveen.jirra@gmail.com>
2019-10-17 11:30:21 +0530

    Updated jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -9,6 +9,6 @@ sudo apt-get install openjdk-8-jdk -y
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 service jenkins start
 service jenkins status
